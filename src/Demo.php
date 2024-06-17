4cc9b2678eed015cebe1330f3c1fa598283a4125
terrafrost <terrafrost@gmail.com>
2024-06-17 04:01:50 -0500

    initil commit

---
+++ src/Demo.php
@@ -0,0 +1,15 @@
+<?php
+namespace Terrafrost/Namespace;
+
+class SSH2
+{
+    public function __construct()
+    {
+        define('ZZZ', 999);
+    }
+
+    public function __destruct()
+    {
+        echo ZZZ . "\n";
+    }
+}
